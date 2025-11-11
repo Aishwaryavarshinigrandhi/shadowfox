1c6436467995a76b15078d1b05e8d8953bd46c6a
Aishwarya varshini Grandhi <aishwaryavarshinigrandhi@gmail.com>
2025-11-11 20:31:50 +0530

    Add files via upload

---
+++ var.py
@@ -0,0 +1,14 @@
+pi = 22 / 7
+print("Value of pi:", pi)
+print("Data type of pi:", type(pi))
+
+
+#for = 4  # ❌ This will raise SyntaxError because 'for' is a reserved keyword in Python.
+print("‘for’ cannot be used as a variable name because it is a reserved keyword.")
+
+P = 10000  # Principal
+R = 5      # Rate (%)
+T = 3      # Time (years)
+simple_interest = P * R * T / 100
+print("Simple Interest for 3 years:", simple_interest)
+
